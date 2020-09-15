537549d380f3f4e069ddea9102ab061a92846333
loyaloy <loyaloy@163.com>
2020-09-15 13:00:46 +0800

    添加项目文件。

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoreVueElementUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
